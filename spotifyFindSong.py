aba8d3b5ffd90e001b9bdb542713614ee321d94d
Nathan Narasimhan <nathan.a.narasimhan@gmail.com>
2017-01-21 14:37:18 -0500

    Finds song on Spotify

--- spotifyFindSong.py
+++ spotifyFindSong.py
@@ -3,4 +3,4 @@ def findSong(s):
     a = s.replace(" ", "+")
     url = "https://api.spotify.com/v1/search?query=" + a + "&type=track&market=US&offset=0&limit=20"
     webbrowser.open_new(url)
-#findSong("Money Pink Floyd")
+#opens the webpage to search spotify
